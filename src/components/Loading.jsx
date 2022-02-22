99177e259d62c5446c3c035c424fe3ca04994502
chickenCHRISpy <chrislopes1718@gmail.com>
2022-02-22 22:41:40 +0530

    first

---
+++ src/components/Loading.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Circles} from 'react-loader-spinner';
+
+export const Loading = () => (
+  <div className="flex justify-center items-center ">
+    <Circles color="#00BFFF" height={550} width={80} />
+  </div>
+);
